299c77a0cdf6c3e23d71acea7ccb4c19be1d508b
wanfer3434 <91153957+wanfer3434@users.noreply.github.com>
2025-04-05 16:32:09 -0500

    Crear productR.dart

---
+++ lib/screens/product/components/productR.dart
@@ -0,0 +1,45 @@
+class ProductResponse {
+  final String id;
+  final String nombre;
+  final String tipo;
+  final String color;
+  final double precio;
+  final String fechaAgregado;
+  final String? imagenUrl;
+
+  ProductResponse({
+    required this.id,
+    required this.nombre,
+    required this.tipo,
+    required this.color,
+    required this.precio,
+    required this.fechaAgregado,
+    this.imagenUrl,
+  });
+
+  factory ProductResponse.fromJson(Map<String, dynamic> json) {
+    return ProductResponse(
+      id: json['id'].toString(),
+      nombre: json['nombre'] ?? '',
+      tipo: json['tipo'] ?? '',
+      color: json['color'] ?? '',
+      precio: (json['precio'] is int)
+          ? (json['precio'] as int).toDouble()
+          : (json['precio'] ?? 0.0).toDouble(),
+      fechaAgregado: json['fechaAgregado'] ?? '',
+      imagenUrl: json['imagenUrl'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'nombre': nombre,
+      'tipo': tipo,
+      'color': color,
+      'precio': precio,
+      'fechaAgregado': fechaAgregado,
+      'imagenUrl': imagenUrl,
+    };
+  }
+}
